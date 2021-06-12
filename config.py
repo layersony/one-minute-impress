fcc2250e0bb538dde8d620168b6815e0cceff28b
layersony <sammaingi5@gmail.com>
2021-06-12 11:30:33 +0300

    shell and database link add

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ import os
 class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 class ProdConfig(Config):
   pass
 
 class DevConfig(Config):
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://layersony:1q2w3e4r5t6y@localhost/oneminipress'
   DEBUG = True
 
 config_options = {'development':DevConfig, 'production':ProdConfig}

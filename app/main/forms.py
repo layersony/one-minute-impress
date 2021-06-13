d14ef9ac49b480eae5037b8bbedf67bc09a44ec3
layersony <sammaingi5@gmail.com>
2021-06-13 13:04:31 +0300

    comment form and vote form add

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,21 @@
-from flask_wtf import FlaskForm # will help us create form
-from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from flask_wtf import FlaskForm, Form # will help us create form
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, RadioField
 from wtforms.validators import Required
 
 class PitchForm(FlaskForm):
   title = StringField('Pitch title', validators=[Required()])
   content = TextAreaField('Pitch content', validators=[Required()])
-  category = SelectField('Type', choices=[('pickuplines','PickUp Lines'), ('inspirations','Inspirations'), ('slogan', 'Slogan')])
+  category = SelectField('Type', choices=[('-----','-----'),('pickuplines','PickUp Lines'), ('inspirations','Inspirations'), ('slogan', 'Slogan')])
   submit = SubmitField('Submit')
+
+class UpdateForm(FlaskForm):
+  bio = TextAreaField('Text us about you', validators=[Required()])
+  submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+  comment = TextAreaField('Speak out Your Mind')
+  submit = SubmitField('Submit')
+
+class VoteForm(Form):
+  vote = RadioField('Vote', choices=[('upvote', 'UpVote'), ('downvote', 'DownVote')])
+  submit = SubmitField('Submit Vote')

60240dc9c5c50ac90a605a94594281ac3773d76f
layersony <sammaingi5@gmail.com>
2021-06-13 13:14:37 +0300

    role table remove

--- app/models.py
+++ app/models.py
@@ -1,18 +1,17 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
-from datetime import datetime
+from . import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
-  __tablename__ = 'user'
+class User(UserMixin, db.Model):
+  __tablename__ = 'users'
 
   id= db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(255))
   email = db.Column(db.String(255))
   profile_pic = db.Column(db.String())
   pass_secure = db.Column(db.String(255))
-  role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-  comments = db.relationship('Comment', backref='user', lazy= 'dynamic')
+  comments = db.relationship('Comment', backref='users', lazy= 'dynamic')
   date_join = db.Column(db.DateTime, default = datetime.utcnow())
 
   @property
@@ -26,18 +25,10 @@ class User(db.Model):
   def verify_password(self, password):
     return check_password_hash(self.pass_secure, password)
 
+
+
   def __repr__(self):
     return f'User {self.username}'
-  
-class Role(db.Model):
-  __tablename__ = 'roles'
-
-  id = db.Column(db.Integer, primary_key = True)
-  name = db.Column(db.String(255))
-  users = db.relationship('User', backref='roles', lazy='dynamic')
-
-  def __repr__(self):
-    return f'User {self.name}'
 
 class Pitch(db.Model):
   __tablename__ = 'pitch'
@@ -48,22 +39,24 @@ class Pitch(db.Model):
   posted = db.Column(db.DateTime, default=datetime.utcnow())
   likes = db.Column(db.Integer)
   dislikes = db.Column(db.Integer)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
   comments = db.relationship('Comment', backref='pitch', lazy='dynamic')
 
   def save_pitch(self):
     db.session.add(self)
     db.session.commit()
-
-  
 class Comment(db.Model):
   __tablename__ = 'comments'
 
   id = db.Column(db.Integer, primary_key = True)
   comment = db.Column(db.String(2000))
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
   pitch_id = db.Column(db.Integer, db.ForeignKey('pitch.id'))
 
   def save_comment(self):
     db.session.add(self)
     db.session.commit()
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))

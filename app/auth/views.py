d4f0bf8d99f2a227ac43c1909df126b6c217256a
layersony <sammaingi5@gmail.com>
2021-06-14 17:33:36 +0300

    redirect to profile

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from . import auth
 from . forms import LoginForm, Registration
 from ..models import User
 from .. import db
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 from ..email import mail_message
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -14,7 +14,7 @@ def login():
     user = User.query.filter_by(email = login_form.email.data).first()
     if user is not None and user.verify_password(login_form.password.data):
       login_user(user, login_form.remember.data)
-      return redirect(request.args.get('next') or url_for('main.index'))
+      return redirect(request.args.get('next') or url_for('main.profile', uname=current_user.username))
     flash('Invalid username or password')
   title = 'Login'
   

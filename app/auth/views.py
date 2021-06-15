479d2aa32317dd08e45e00932d691cb72872d112
layersony <sammaingi5@gmail.com>
2021-06-15 10:57:57 +0300

    email deloyment

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,7 +28,7 @@ def register():
     db.session.add(user)
     db.session.commit()
 
-    mail_message("Welcome to One Minute Impress", "email/welcome_user", user.email, user=user)
+    # mail_message("Welcome to One Minute Impress", "email/welcome_user", user.email, user=user)
 
     return redirect(url_for('auth.login'))
 
